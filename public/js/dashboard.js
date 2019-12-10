2d1677d2f67d84461ae7be6ba5c5e8ac534a2712
nbdunks <thenicoleduncan@gmail.com>
2019-12-09 23:10:52 -0700

    added completed and delte buttons, they still need logic

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,2 +1,9 @@
 console.log("dashboard.js is connected"); 
 
+// (document).ready(function(){
+//     $(document).on("click", "button.delete", deleteTodo);
+//     $(document).on("click", "button.complete", toggleComplete);
+
+
+
+// });
