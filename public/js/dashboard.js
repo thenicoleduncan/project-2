b204744b064346624c7d8bb6784853d2e420afdd
Miyoung Chung <mkchung.8@gmail.com>
2019-12-10 09:28:55 -0700

    added update and delete functions for goals

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,41 @@
-console.log("dashboard.js is connected"); 
+$(document).ready(function () {
+    console.log("dashboard.js is connected");
 
-// (document).ready(function(){
-//     $(document).on("click", "button.delete", deleteTodo);
-//     $(document).on("click", "button.complete", toggleComplete);
+    $(".goaldelete").on("click", function (event) {
+        event.preventDefault();
+        console.log(`delete button clicked at id ${$(this).data("goalid")}`);
+        let id = $(this).data("goalid");
+        let dataObj = {
+            GoalId: id
+        };
+        $
+            .ajax(`/api/goal/${id}`, {
+                type: "DELETE",
+                data: dataObj
+            }).then(function () {
+                console.log(`goal ${id} deleted`);
+                location.reload();
+            })
+    });
+
+    $(".goaldone").on("click", function (event) {
+        event.preventDefault();
+        console.log(`goal done button clicked at id ${$(this).data("goalid")}`);
+        let id = $(this).data("goalid");
+        let dataObj = {
+            completed: 1
+        };
+        $
+            .ajax(`/api/goal/${id}`, {
+                type: "PUT",
+                data: dataObj
+            }).then(function () {
+                console.log(`goal ${id} completed`);
+                location.reload();
+            });
+    }); 
 
 
+}); 
 
-// });
+
